6f7c819127a54b1d01e1d07b0cbac794c32fb990
F0sya <nikitachmyria@gmail.com>
2024-04-20 03:02:26 +0300

    The laboratory exercise 1 was done

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,20 +4,9 @@ using namespace std;
 
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 0x04);
-	cout << "Every" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0C);
-	cout << "\tHunter" << endl;
 	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "\t\tWants" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0A);
-	cout << "\t\t\tTo know" << endl;
-	SetConsoleTextAttribute(hConsole, 0x09);
-	cout << "\t\t\t\tWhere" << endl;
-	SetConsoleTextAttribute(hConsole, 0x01);
-	cout << "\t\t\t\t\tSits" << endl;
-	SetConsoleTextAttribute(hConsole, 0x05);
-	cout << "\t\t\t\t\t\tPheasant" << endl;
+	cout << "Hello,World!" << endl;
 	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "/Bjarne Stroustrup/" << endl;
 	return 0;
 }
