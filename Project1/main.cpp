936765ecb6f0bdde34b74aca4c29297783b933d8
F0sya <nikitachmyria@gmail.com>
2024-04-20 03:16:42 +0300

    The homework exercise 3 was done

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,21 +3,28 @@
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 0x04);
-	cout << "Every" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0C);
-	cout << "\tHunter" << endl;
+	cout << "Продам цуценят.\n"
+		"Порода: ротвейлер.\n"
+		"……\n"
+		"Тел.: 222-22-22\n"
+		"222\t|";
 	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "\t\tWants" << endl;
-	SetConsoleTextAttribute(hConsole, 0x0A);
-	cout << "\t\t\tTo know" << endl;
-	SetConsoleTextAttribute(hConsole, 0x09);
-	cout << "\t\t\t\tWhere" << endl;
-	SetConsoleTextAttribute(hConsole, 0x01);
-	cout << "\t\t\t\t\tSits" << endl;
-	SetConsoleTextAttribute(hConsole, 0x05);
-	cout << "\t\t\t\t\t\tPheasant" << endl;
+	cout << "   222   ";
 	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "|\n"
+		"22\t|";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "   22    ";
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "|\n"
+		"22\t|";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "   22    ";
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "|\n"
+		"і т.д." << endl;
 	return 0;
 }
